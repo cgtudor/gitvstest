ff49889c26fc7550656ca3300f17a3682c7d33c3
Cristian <tudor.cristian.gabriel@gmail.com>
2019-09-24 14:03:06 +0100

    Awesome commit

--- GitVisualStudioTest/Program.cs
+++ GitVisualStudioTest/Program.cs
@@ -6,7 +6,7 @@ namespace GitVisualStudioTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("MS VS is awesome!");
         }
     }
 }
